eb6758fec961aeec9c4b119a0872226be46698f9
Ilya Kizhvatov <i.kizhvatov@cs.ru.nl>
2017-05-16 15:03:27 +0200

    extra jlsca configurations

---
+++ testcase01/main-inccpa.jl
@@ -0,0 +1,53 @@
+
+using Jlsca.Sca
+using Jlsca.Trs
+using Jlsca.Align
+
+# our vanilla  main function
+function gofaster()
+  if length(ARGS) < 1
+    @printf("no input trace\n")
+    return
+  end
+
+  filename = ARGS[1]
+  direction::Direction = (length(ARGS) > 1 && ARGS[2] == "BACKWARD" ? BACKWARD : FORWARD)
+  params = getParameters(filename, direction)
+  if params == nothing
+    params = AesSboxAttack()
+  end
+
+  params.analysis = IncrementalCPA()
+  #params.analysis.leakageFunctions = [hw]
+  params.analysis.leakageFunctions = [x -> ((x .>> i) & 1) for i in 0:7]
+
+  numberOfAverages = length(params.keyByteOffsets)
+  numberOfCandidates = getNumberOfCandidates(params)
+
+  @everyworker begin
+      using Jlsca.Trs
+      using Jlsca.Align
+
+      trs = InspectorTrace($filename)
+
+      # # example alignment pass
+      # maxShift = 20000
+      # referenceOffset = 5000
+      # reference = trs[1][2][referenceOffset:referenceOffset+5000]
+      # corvalMin = 0.4
+      # alignstate = CorrelationAlignFFT(reference, referenceOffset, maxShift)
+      # addSamplePass(trs, x -> ((shift,corval) = correlationAlign(x, alignstate); corval > corvalMin ? circshift(x, shift) : Vector{eltype(x)}(0)))
+
+      setPostProcessor(trs, IncrementalCorrelation(SplitByData($numberOfAverages, $numberOfCandidates)))
+      # setPostProcessor(trs, IncrementalCorrelation(SplitByTracesBlock()))
+      # setPostProcessor(trs, IncrementalCorrelation(SplitByTracesSliced()))
+  end
+
+  numberOfTraces = @fetch length(Main.trs)
+
+  ret = sca(DistributedTrace(), params, 1, numberOfTraces)
+
+  return ret
+end
+
+@time gofaster()
---
+++ testcase01/main-noninc.jl
@@ -0,0 +1,45 @@
+using Jlsca.Sca
+using Jlsca.Trs
+using Jlsca.Align
+
+# our vanilla  main function
+function go()
+  if length(ARGS) < 1
+    @printf("no input trace\n")
+    return
+  end
+
+  if nworkers() > 1
+    @printf("This module does not use parallelization!")
+  end
+
+  filename = ARGS[1]
+  direction::Direction = (length(ARGS) > 1 && ARGS[2] == "BACKWARD" ? BACKWARD : FORWARD)
+  params = getParameters(filename, direction)
+  if params == nothing
+    params = AesSboxAttack()
+  end
+
+  #params.analysis.leakageFunctions = [hw]
+  params.analysis.leakageFunctions = [x -> ((x .>> i) & 1) for i in 0:7]
+  numberOfAverages = length(params.keyByteOffsets)
+  numberOfCandidates = getNumberOfCandidates(params)
+
+  trs = InspectorTrace(filename)
+
+  # # example alignment pass
+  # maxShift = 20000
+  # referenceOffset = 5000
+  # reference = trs[1][2][referenceOffset:referenceOffset+5000]
+  # corvalMin = 0.4
+  # alignstate = CorrelationAlignFFT(reference, referenceOffset, maxShift)
+  # addSamplePass(trs, x -> ((shift,corval) = correlationAlign(x, alignstate); corval > corvalMin ? circshift(x, shift) : Vector{eltype(x)}(0)))
+
+  numberOfTraces = length(trs)
+
+  ret = sca(trs, params, 1, numberOfTraces)
+
+  return ret
+end
+
+@time go()
