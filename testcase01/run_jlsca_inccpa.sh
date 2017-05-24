ff7a903383d2c33ae80ac99312660327d659b7ed
Cees-Bart Breunesse <ceesb@riscure.com>
2017-05-23 20:50:11 -0700

    speeeeeeed

--- testcase01/run_jlsca_inccpa.sh
+++ testcase01/run_jlsca_inccpa.sh
@@ -1,1 +1,1 @@
-JULIA_NUM_THREADS=2 \time julia main-inccpa.jl aes128_sb_ciph_deadbeefcafebabe1122334455667788.trs
+JULIA_NUM_THREADS=$(($(nproc --all)/2)) time julia main-inccpa.jl aes128_sb_ciph_deadbeefcafebabe1122334455667788.trs
